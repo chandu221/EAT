95684f309f8d403b630bb2c4fe847822094dff74
chandu221 <149949983+chandu221@users.noreply.github.com>
2025-05-05 14:52:29 +0530

    Add files via upload

---
+++ Equity_Award_Tracker/Backend/Apilayer/Models/QouteResponse.cs
@@ -0,0 +1,14 @@
+using System.Text.Json.Serialization;
+ 
+namespace ExternalApiMock.Models;
+ 
+public class QuoteResponse
+{
+    [JsonPropertyName("c")] public double Current { get; set; }
+    [JsonPropertyName("d")] public double Change { get; set; }
+    [JsonPropertyName("dp")] public double PercentChange { get; set; }
+    [JsonPropertyName("h")] public double High { get; set; }
+    [JsonPropertyName("l")] public double Low { get; set; }
+    [JsonPropertyName("o")] public double Open { get; set; }
+    [JsonPropertyName("pc")] public double PreviousClose { get; set; }
+}
