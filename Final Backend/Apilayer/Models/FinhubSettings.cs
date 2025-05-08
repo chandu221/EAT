ca9cca3bc1b4fb174ab74e0ae0c61834599bdc5c
chandu221 <149949983+chandu221@users.noreply.github.com>
2025-05-08 10:48:34 +0530

    Delete Final Backend/Apilayer directory

--- Final Backend/Apilayer/Models/FinhubSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace FinhubApiLayer.Models
-{
-    public class FinnhubSettings
-    {
-        public string BaseUrl { get; set; }
-        public string ApiKey { get; set; }
-    }
-}
--- Final Backend/Apilayer/Services/IexternalApiService.cs
+++
@@ -1,8 +0,0 @@
-using ExternalApiMock.Models;
- 
-namespace ExternalApiMock.Services;
- 
-public interface IExternalApiService
-{
-    Task<QuoteResponse?> GetQuoteAsync(string symbol);
-}
